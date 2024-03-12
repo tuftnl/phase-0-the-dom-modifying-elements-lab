601b1df4c3c241d3877bc2cd027532ce3b5dfdc9
Aaron Lin <aaron.l.lin.general@gmail.com>
2024-03-12 16:49:40 -0400

    first attempt

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = ('Name is the champion');
+document.body.append(newHeader);
